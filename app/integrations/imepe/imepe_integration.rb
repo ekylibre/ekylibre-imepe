6e2346a6d135b9c20760a9e47a5fe0d6441511a1
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-05-23 19:05:11 +0200

    Laying down the ground work on the integration before we get the XML value.

--- app/integrations/imepe/imepe_integration.rb
+++ app/integrations/imepe/imepe_integration.rb
@@ -5,19 +5,31 @@ module Imepe
     end
     calls :debug
 
+    SERVER = "www.rhone-alpes.test.mesparcelles.fr"
+    API_VERSION = '1.4'
+    FORMAT = :xml
+
     def debug
-      integration = fetch
-      get_json("http://gateway.cloud.imepe.com/api/devices?access-token=#{integration.parameters['access_token']}") do |r|
+      # integration = fetch
+      get_format(url(:siga_web, :exploitations)) do |r|
+        byebug
       end
     end
 
     def check(integration = nil)
       integration = fetch integration
-      get_json("http://gateway.cloud.imepe.com/api/access_tokens/#{integration.parameters['access_token']}") do |r|
-        r.success do
-          Rails.logger.info 'CHECKED'.green
-        end
-      end
+      get_format(url(:siga_web, :exploitations))
+      # TODO: Add check on SIRET
+    end
+
+    private
+
+    def url(application, service, server: SERVER, version: VERSION, format: FORMAT)
+      "http://#{server}/apiService/#{version}/#{application}/#{service}.#{format}"
+    end
+
+    def get_format(*args, **kwargs, &block)
+      send(:"get_#{FORMAT}", *args, **kwargs, &block)
     end
   end
 end
