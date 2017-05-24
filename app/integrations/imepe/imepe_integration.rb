db2a74ce8c29f3e26f167010b4222e12435d7bbd
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-05-24 12:29:30 +0200

    Completed the #check and overall thickened the integration.

--- app/integrations/imepe/imepe_integration.rb
+++ app/integrations/imepe/imepe_integration.rb
@@ -1,25 +1,30 @@
 module Imepe
   class ImepeIntegration < ActionIntegration::Base
     auth :check do
-      parameter :siret_number
+      parameter :siret
     end
     calls :debug
 
     SERVER = "www.rhone-alpes.test.mesparcelles.fr"
-    API_VERSION = '1.4'
+    VERSION = '1.4'
     FORMAT = :xml
 
     def debug
       # integration = fetch
-      get_format(url(:siga_web, :exploitations)) do |r|
+      get_format(url(:siga_web, :exploitations), headers) do |r|
         byebug
       end
     end
 
     def check(integration = nil)
       integration = fetch integration
-      get_format(url(:siga_web, :exploitations))
-      # TODO: Add check on SIRET
+      get_format(url(:siga_web, :exploitations), headers) do |r|
+        r.success do
+          sirets = Nokogiri::XML(r.body).css('exploitations exploitation identification siret').map(&:inner_text)
+          there = sirets.include? integration.parameters['siret']
+          there || error
+        end
+      end
     end
 
     private
@@ -28,11 +33,30 @@ module Imepe
       "http://#{server}/apiService/#{version}/#{application}/#{service}.#{format}"
     end
 
-    def get_format(*args, **kwargs, &block)
-      major_version = VERSION.split('.').to_i
+    def headers
+      username = 'Ekylibre'
+      password = 'AiEcpa'
+
+      nonce = rand(2**256).to_s(36)[0..7]
+      created_at = Time.now.strftime("%Y-%m-%dT%TZ")
+
+      hashed_password = Digest::SHA256.hexdigest(password)
+      token = nonce + created_at + hashed_password
+      digest = Digest::SHA1.hexdigest(token)
+      digest = Base64.strict_encode64(digest)
+
+      {
+        "Authorization" => 'WSSE profile="UsernameToken"',
+        "X-WSSE" => "UsernameToken Username=\"#{username}\", PasswordDigest=\"#{digest}\", Nonce=\"#{nonce}\", Created=\"#{created_at}\""
+      }
+    end
+
+    def get_format(*args, &block)
+      major_version = VERSION.split('.').first.to_i
       format = FORMAT.downcase.to_sym
       Rails.logger.warn 'IMEPE API v1 only supports the XML format !' if major_version <= 1 && format != :xml
-      send(:"get_#{FORMAT}", *args, **kwargs, &block)
+      byebug
+      send(:"get_#{FORMAT}", *args, &block)
     end
   end
 end
